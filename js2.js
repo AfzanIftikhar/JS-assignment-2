915a038f51243f0e4d6918f215f9d0825498ad7d
AfzanIftikhar <aafzan95@gmail.com>
2024-09-19 10:33:30 -0700

    jsa

---
+++ js2.js
@@ -0,0 +1,50 @@
+//..........Task 1.........
+
+console.log(Math.round(prompt('Enter a number') / 10) * 10);
+console.log(Math.round(prompt('Enter a number') / 100) * 100);
+
+
+
+//........Task 2........
+
+console.log(Math.random()*100)
+console.log(Math.random()*100)
+console.log(Math.random()*100)
+console.log(Math.random()*100)
+console.log(Math.random()*100)
+
+
+
+//.........Task 3..........
+
+
+let num1 = "123"
+let result = parseInt(num1);
+console.log(result)
+
+let num2 = "45.65"
+let result2 = parseFloat(num2);
+console.log(result2)
+
+let num3 = "89.01"
+let result3 = parseFloat(num3);
+console.log(result3)
+
+
+let num4 = "100"
+let result4 = parseFloat(num4);
+console.log(result4)
+
+
+
+//........Task 4.........
+
+let x = Math.floor(Math.random()*(50 - 10 + 1) + 10   )
+console.log(x)
+
+
+//.........Task 5......
+
+let y = "300"
+    r = parseInt(y);
+    console.log(r)
